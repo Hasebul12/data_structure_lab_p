c33aa397ebc48844e5dd82fe7d67b9f78501a918
Hasebul12 <113965513+Hasebul12@users.noreply.github.com>
2022-10-11 13:52:10 +0600

    Add files via upload

---
+++ Lab3 ascii conversion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ char x;
+ cout << "Enter a character: ";
+ cin >> x;
+ cout<< "ASCII" << x << " is " <<int(x+2);
+ return 0;
+}
